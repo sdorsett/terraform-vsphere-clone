cc5c928d99c3729f0a794d9a85c5eea0d758d33f
Stan Dorsett <stan.dorsett@gmail.com>
2018-12-24 21:43:22 +0100

    added main.tf, output.tf and variables.tf files

---
+++ output.tf
@@ -0,0 +1,7 @@
+output "ip" { 
+   value = "${vsphere_virtual_machine.cloned_virtual_machine.*.default_ip_address}" 
+}
+output "vm-moref" {
+   value = "${vsphere_virtual_machine.cloned_virtual_machine.moid}"
+}
+
